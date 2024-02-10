e1a473e9e4203543210e08acd0b037d76e31d241
codezoneo <anuragtri83@gmail.com>
2024-02-10 14:03:57 +0530

    AboutUs

--- src/Components/SearchItem.js
+++ src/Components/SearchItem.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import items from "./components/Data";
+import Product from "./Components/Product";
 
-const SearchItem = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const SearchItem = ({ cart, setCart }) => {
+  // console.log(useParams())
+  const { term } = useParams();
+  const [filterData, setFilterData] = useState([]);
+
+  useEffect(() => {
+    const filteredData = () => {
+      const data = items.filter((p) =>
+        p.title.toLowerCase().includes(term.toLowerCase())
+      );
+      // console.log(data)
+      setFilterData(data);
+    };
+
+    filteredData();
+  }, [term]);
+
+  return <Product cart={cart} setCart={setCart} items={filterData} />;
+};
 
 export default SearchItem;
